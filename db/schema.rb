e21a68327e39965d56e9f31863400b3be749199a
Carolyn Cole <cac9@princeton.edu>
2020-03-27 13:38:24 -0400

    Adding a DataSet scaffold     ran `rails g scaffold DataSet`
        Just added this so there is a page for production to show.  The default rails page does not show in production and staging.
        To remove these files run `rails d scaffold DataSet` if we decide this is not a good model for the project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_27_172734) do
+
+  create_table "data_sets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ test/system/data_sets_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class DataSetsTest < ApplicationSystemTestCase
+  setup do
+    @data_set = data_sets(:one)
+  end
+
+  test "visiting the index" do
+    visit data_sets_url
+    assert_selector "h1", text: "Data Sets"
+  end
+
+  test "creating a Data set" do
+    visit data_sets_url
+    click_on "New Data Set"
+
+    click_on "Create Data set"
+
+    assert_text "Data set was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Data set" do
+    visit data_sets_url
+    click_on "Edit", match: :first
+
+    click_on "Update Data set"
+
+    assert_text "Data set was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Data set" do
+    visit data_sets_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Data set was successfully destroyed"
+  end
+end
