5168ea7c37238f2c660578f293ea526299684071
Carolyn Cole <cac9@princeton.edu>
2020-11-20 13:35:29 -0500

    Adding production deploy Also updating ruby to match the server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server "lib-jobs-prod1.princeton.edu", user: "deploy", roles: %w[app db web]
 
 # role-based syntax
 # ==================
