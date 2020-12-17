8b650be0e5e32d39bbbdece971e327033134db45
James R. Griffin III <jrgriffiniii@gmail.com>
2020-12-17 13:08:19 -0500

    Restructuring the markup and improving the styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,62 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
-  helper_method :application_name, :current_year
+  helper_method :application_name, :current_year, :library_header_menu_items
 
   def application_name
-    "Lib Jobs"
+    t(:application_name)
   end
 
   def current_year
-    Date.today.year
+    Time.zone.today.year
+  end
+
+  def header_user_menu_item
+    if current_user
+      {
+        name: current_user.email,
+        component: current_user.email,
+        href: '#',
+        children: [
+          {
+            name: 'Log Out',
+            component: 'Log Out',
+            href: destroy_user_session_path
+          }
+        ]
+      }
+    else
+      {
+        name: 'Log In',
+        component: 'Log In',
+        href: new_user_session_path
+      }
+    end
+  end
+
+  def library_header_menu_items
+    [
+      {
+        name: 'Data Sets',
+        component: 'Data Sets',
+        href: data_sets_path
+      },
+      {
+        name: 'Staff Directory',
+        component: 'Staff Directory',
+        href: staff_directory_path
+      },
+      {
+        name: 'Absolute IDs',
+        component: 'Absolute IDs',
+        href: absolute_ids_path
+      },
+      header_user_menu_item
+    ]
+  end
+
+  def library_header_attributes
+    {
+      'menu-items': header_menu_items
+    }
   end
 end
