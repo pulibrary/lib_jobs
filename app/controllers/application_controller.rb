6d7a479f042b05506258743b87d58ad975d7122e
James R. Griffin III <jrgriffiniii@gmail.com>
2020-12-17 00:54:02 -0500

    Integrating LUX for the user interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  helper_method :application_name, :current_year
+
+  def application_name
+    "Lib Jobs"
+  end
+
+  def current_year
+    Date.today.year
+  end
 end
