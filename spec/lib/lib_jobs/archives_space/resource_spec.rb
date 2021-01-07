78e97e9b2d7a8bd70ff233debc591944607a336a
James R. Griffin III <jrgriffiniii@gmail.com>
2021-01-07 09:00:55 -0500

    Restructuring the code base for the ArchivesSpace client

---
+++ spec/lib/lib_jobs/archives_space/resource_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe LibJobs::ArchivesSpace::Resource do
+  subject(:resource) do
+    described_class.new(repository, fixture_values)
+  end
+  let(:top_container) { instance_double(LibJobs::ArchivesSpace::TopContainer) }
+  let(:repository) { instance_double(LibJobs::ArchivesSpace::Repository) }
+  let(:fixture_file_path) do
+    Rails.root.join('spec', 'fixtures', 'archivesspace_resource.json')
+  end
+  let(:fixture_json) do
+    File.read(fixture_file_path)
+  end
+  let(:fixture_values) do
+    JSON.parse(fixture_json)
+  end
+
+  before do
+    allow(repository).to receive(:find_top_container).and_return(top_container)
+  end
+
+  describe '#instances' do
+    it 'retrieves the instances' do
+      expect(resource.instances).not_to be_empty
+      expect(resource.instances.first).to be_a LibJobs::ArchivesSpace::Instance
+      expect(resource.instances.first.sub_container).to be_a LibJobs::ArchivesSpace::SubContainer
+      expect(resource.instances.first.top_container).to be top_container
+      expect(resource.instances.first.sub_container.top_container).to be top_container
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,7 @@ require 'webmock/rspec'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('lib', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
