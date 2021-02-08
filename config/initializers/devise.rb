28d819749dec91bf92580188f31888af72ed363e
James R. Griffin III <jrgriffiniii@gmail.com>
2021-02-08 10:45:58 -0500

    Trying another fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth(:cas, host: Rails.application.config.cas.host, url: Rails.application.config.cas.url)
+  config.omniauth :cas, host: "fed.princeton.edu", url: "https://fed.princeton.edu/cas"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
