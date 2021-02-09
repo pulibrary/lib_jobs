427ad748f81eb4aea6c9f01d83ba458547921768
James R. Griffin III <jrgriffiniii@gmail.com>
2021-02-09 12:30:27 -0500

    [WIP] More refactoring

--- app/models/absolute_id/container_profile.rb
+++ app/models/absolute_id/container_profile.rb
@@ -3,4 +3,11 @@ class AbsoluteId::ContainerProfile < AbsoluteId::Record
   def self.resource_class
     LibJobs::ArchivesSpace::ContainerProfile
   end
+
+  def attributes
+    super.merge({
+      name: json_resource.name,
+      prefix: json_resource.prefix
+    })
+  end
 end
