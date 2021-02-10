2bbcf32ca0d1d1c289c139538e4a42a5f7edefb4
James R. Griffin III <jrgriffiniii@gmail.com>
2021-02-10 11:21:51 -0500

    Fixing bugs found for the CSV import

--- lib/lib_jobs/archives_space.rb
+++ lib/lib_jobs/archives_space.rb
@@ -2,8 +2,9 @@
 module LibJobs
   module ArchivesSpace
     autoload(:Object, File.join(File.dirname(__FILE__), 'archives_space', 'object'))
-    autoload(:TopContainer, File.join(File.dirname(__FILE__), 'archives_space', 'top_container'))
+    autoload(:ChildObject, File.join(File.dirname(__FILE__), 'archives_space', 'child_object'))
     autoload(:SubContainer, File.join(File.dirname(__FILE__), 'archives_space', 'sub_container'))
+    autoload(:TopContainer, File.join(File.dirname(__FILE__), 'archives_space', 'top_container'))
     autoload(:Instance, File.join(File.dirname(__FILE__), 'archives_space', 'instance'))
     autoload(:Resource, File.join(File.dirname(__FILE__), 'archives_space', 'resource'))
     autoload(:Repository, File.join(File.dirname(__FILE__), 'archives_space', 'repository'))
