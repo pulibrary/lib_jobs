98c92f4f1d925866b43bd821d2f686fa6aaf42a5
Carolyn Cole <cac9@princeton.edu>
2021-02-18 09:54:31 -0500

    Adding path to crontab avoids `/bin/bash: bundle: command not found` error

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 set :output, '/opt/lib-jobs/shared/tmp/cron_log.log'
+env :PATH, ENV["PATH"]
 
 # Run at 6:05 am EST or 7:05 EDT (after the 6am staff report is generated)
 every 1.day, at: '11:05 am' do
