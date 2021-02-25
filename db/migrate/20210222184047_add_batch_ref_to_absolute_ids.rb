dff6b920f1f862d8fc11a7db543ee19eca23adc8
James R. Griffin III <jrgriffiniii@gmail.com>
2021-02-25 09:28:43 -0500

    [WIP] db migration

--- db/migrate/20210222184047_add_batch_ref_to_absolute_ids.rb
+++ db/migrate/20210222184047_add_batch_ref_to_absolute_ids.rb
@@ -1,5 +1,5 @@
 class AddBatchRefToAbsoluteIds < ActiveRecord::Migration[5.2]
   def change
-    add_reference :absolute_ids, :batch, foreign_key: true
+    add_reference :absolute_ids, :absolute_ids_batch, foreign_key: true
   end
 end
