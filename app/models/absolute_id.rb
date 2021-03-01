848b4934974a91dc67c11841974d551f0f7f15da
James R. Griffin III <jrgriffiniii@gmail.com>
2021-03-01 05:03:55 -0500

    Fixing model names

--- app/models/absolute_id.rb
+++ app/models/absolute_id.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AbsoluteId < ApplicationRecord
-  belongs_to :batch, optional: true
+  belongs_to :absolute_id_batch, optional: true
 
   def self.barcode_model
     AbsoluteIds::Barcode
