7ba4defed8fd09d5e1f718bac0acc2e2ecbb481f
James R. Griffin III <jrgriffiniii@gmail.com>
2021-03-01 05:46:27 -0500

    Trying to fix the model validation again

--- app/models/absolute_id.rb
+++ app/models/absolute_id.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AbsoluteId < ApplicationRecord
-  belongs_to :absolute_id_batch, optional: true
+  belongs_to :batch, class_name: 'AbsoluteId::Batch', optional: true
 
   def self.barcode_model
     AbsoluteIds::Barcode
