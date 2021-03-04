2ea5363413fa3a4d7ead8bb5027d2a15c1af6f4b
James R. Griffin III <jrgriffiniii@gmail.com>
2021-03-04 12:57:21 -0500

    [WIP] Fixing caching

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ namespace :deploy do
   after :finishing do
     on roles(:app), in: :parallel do
       within release_path do
-        execute :rake, 'lib_jobs:absolute_ids:aspace:cache'
+        execute :rake, 'lib_jobs:absolute_ids:aspace:cache', '--trace'
       end
     end
   end
