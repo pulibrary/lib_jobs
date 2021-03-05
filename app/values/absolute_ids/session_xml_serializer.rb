8d67efaaf1a5eceec610e214a9bc9821c9403f34
James R. Griffin III <jrgriffiniii@gmail.com>
2021-03-05 12:00:41 -0500

    Improving the XML export

--- app/values/absolute_ids/session_xml_serializer.rb
+++ app/values/absolute_ids/session_xml_serializer.rb
@@ -1,26 +1,6 @@
 # frozen_string_literal: true
 module AbsoluteIds
-  class SessionXmlSerializer
-    def initialize(model, _options = {})
-      @model = model
-    end
-
-    def model_element_name
-      "<#{@model.model_name.to_s.underscore} />"
-    end
-
-    def build_document_tree
-      Nokogiri::XML(model_element_name)
-    end
-
-    def document_tree
-      @document_tree ||= build_document_tree
-    end
-
-    def root_element
-      @root_element ||= document_tree.root
-    end
-
+  class SessionXmlSerializer < RecordXmlSerializer
     def build_document
       @document_tree = build_document_tree
 
@@ -32,14 +12,6 @@ module AbsoluteIds
       end
 
       document_tree
-    end
-
-    def document
-      @document ||= build_document
-    end
-
-    def serialize
-      document.to_xml
     end
   end
 end
