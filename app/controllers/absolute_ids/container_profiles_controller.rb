18e8ca597a094f42ea2cdc4d46acdbde2751d826
Trey Pendragon <tpendragon@princeton.edu>
2021-03-18 14:12:54 -0700

    Remove unused authorization code.

--- app/controllers/absolute_ids/container_profiles_controller.rb
+++ app/controllers/absolute_ids/container_profiles_controller.rb
@@ -20,18 +20,6 @@ class AbsoluteIds::ContainerProfilesController < ApplicationController
 
   private
 
-  def value
-    params[:value]
-  end
-
-  def current_user_params
-    params[:user]
-  end
-
-  def current_user_id
-    current_user_params[:id]
-  end
-
   def token_header
     value = request.headers['Authorization']
     return if value.nil?
@@ -41,19 +29,5 @@ class AbsoluteIds::ContainerProfilesController < ApplicationController
 
   def token_header?
     token_header.present?
-  end
-
-  def current_user_token
-    token_header || current_user_params[:token]
-  end
-
-  def find_user
-    User.find_by(id: current_user_id, token: current_user_token)
-  end
-
-  def current_user
-    return super if !super.nil? || current_user_params.nil?
-
-    @current_user ||= find_user
   end
 end
