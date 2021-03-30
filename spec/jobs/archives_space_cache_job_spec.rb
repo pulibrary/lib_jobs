bb42448bdc1576c12dce8ab1e82b1a42159895b9
Trey Pendragon <tpendragon@princeton.edu>
2021-03-30 15:19:56 -0700

    Add testing for caching jobs.
    This removes Resource caching because it wasn't paging resources and I
    don't see a place in the code where having a local copy of all the
    resources is actually necessary.
    
    We may need this when we validate which AbID to put in next, but if so
    having a local copy of every resource will be too intensive anyways.

---
+++ spec/jobs/archives_space_cache_job_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ArchivesSpaceCacheJob do
+  with_queue_adapter :inline
+  it 'creates AR caches of everything it can' do
+    stub_aspace_login
+    stub_repositories
+    # TODO: Fix this bug.
+    # This is a bug - it's only caching the first repository right now because
+    # repository#top_containers just returns existing cached items if there's
+    # any cache at all.
+    stub_repository_top_containers(repository_id: 3)
+    stub_repository(repository_id: 3)
+    stub_repository(repository_id: 4)
+    stub_locations
+    stub_container_profiles
+    described_class.perform_now
+
+    expect(AbsoluteId::Repository.all.size).to eq 2
+    expect(AbsoluteId::Location.all.size).to eq 26
+    expect(AbsoluteId::ContainerProfile.all.size).to eq 13
+    # This number is way too small - it's only one page of one repository.
+    # Do we need to cache these even?
+    expect(AbsoluteId::TopContainer.all.size).to eq 250
+  end
+end
