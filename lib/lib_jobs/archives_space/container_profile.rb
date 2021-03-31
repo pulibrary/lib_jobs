e7b06f8eb65d7514599e9849cb13072bf3689b3f
Trey Pendragon <tpendragon@princeton.edu>
2021-03-31 12:19:12 -0700

    Stop caching ContainerProfiles
    More simplification work.

--- lib/lib_jobs/archives_space/container_profile.rb
+++ lib/lib_jobs/archives_space/container_profile.rb
@@ -3,10 +3,6 @@
 module LibJobs
   module ArchivesSpace
     class ContainerProfile < Object
-      def self.model_class
-        AbsoluteId::ContainerProfile
-      end
-
       attr_accessor :prefix
       attr_reader :name
       def initialize(attributes)
