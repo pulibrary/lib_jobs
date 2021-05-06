2ec88af9e69f75d9f6658d89e8001935179fe44a
Trey Pendragon <tpendragon@princeton.edu>
2021-05-06 11:34:32 -0700

    Convert index to integer type.
    Sorting doesn't work without this.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_220209) do
+ActiveRecord::Schema.define(version: 2021_05_06_183059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_220209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "initial_value"
-    t.string "index"
+    t.integer "index"
     t.string "location"
     t.string "container_profile"
     t.string "repository"
