0ff6320507b2a28072f51ff2b46d413ecf512045
Carolyn Cole <cac9@princeton.edu>
2021-05-14 14:03:09 -0400

    Updating onbase report to have vendor code instead of vendor id Also added vendor code along with vendor id to the status report and email

--- spec/models/voucher_feed_spec.rb
+++ spec/models/voucher_feed_spec.rb
@@ -22,13 +22,13 @@ RSpec.describe VoucherFeed, type: :model do
       expect(sftp_session).to have_received(:rename).with("/alma/invoices/abc.xml", "/alma/invoices/abc.xml.processed")
       data_set = DataSet.last
       expect(data_set.category).to eq("VoucherFeed")
-      expect(data_set.data).to eq("Lib Vendor Invoice Date,Invoice No,Vendor Code,Invoice Amount,Invoice Curency,Local Amount,Voucher ID,Errors\n"\
-                                  "2021-03-30,PO-9999,111222333,1319.05,USD,124.94,A1222333,\"\"\n")
+      expect(data_set.data).to eq("Lib Vendor Invoice Date,Invoice No,Vendor Code,Vendor Id,Invoice Amount,Invoice Curency,Local Amount,Voucher ID,Errors\n"\
+                                  "2021-03-30,PO-9999,XXX,111222333,1319.05,USD,124.94,A1222333,\"\"\n")
       expect(data_set.report_time).to eq(Time.zone.now.midnight)
       data = File.read("/tmp/alma_voucher_#{today}.XML")
       expect(data).to eq(File.open(Rails.root.join('spec', 'fixtures', 'finance_invoice.xml')).read)
       data = File.read("/tmp/Library Invoice Keyword Update_#{onbase_today}.csv")
-      expect(data).to eq("\"2021-03-30\",\"PO-9999\",\"111222333\",\"1319.05\",\"A1222333\"\n")
+      expect(data).to eq("\"2021-03-30\",\"PO-9999\",\"XXX\",\"1319.05\",\"A1222333\"\n")
     end
   end
 end
