494ef88981606d3771b3bd14a23da41277c31c7c
Carolyn Cole <cac9@princeton.edu>
2021-06-22 14:50:23 -0400

    Changing empty to valid invoices stops us from creating empty onbase and peoplesoft files

--- spec/models/peoplesoft_voucher/voucher_feed_spec.rb
+++ spec/models/peoplesoft_voucher/voucher_feed_spec.rb
@@ -54,5 +54,32 @@ RSpec.describe PeoplesoftVoucher::VoucherFeed, type: :model do
       expect(confirm_email.html_part.body.to_s).to include("No errors were found with the invoices")
       expect(confirm_email.html_part.body.to_s).to include("No invoices available to process")
     end
+
+    it "does not generates xml files if no valid invoices are present" do
+      allow(sftp_dir).to receive(:foreach).and_yield(sftp_entry1)
+      # only 1 & 3 should get downloaded
+      allow(sftp_session).to receive(:download!).with("/alma/invoices/abc.xml").and_return(Rails.root.join('spec', 'fixtures', 'invalid_invoice.xml').read)
+      allow(sftp_session).to receive(:rename).with("/alma/invoices/abc.xml", "/alma/invoices/abc.xml.processed")
+      allow(Net::SFTP).to receive(:start).and_yield(sftp_session)
+
+      voucher_feed = described_class.new(onbase_output_base_dir: '/tmp', peoplesoft_output_base_dir: '/tmp')
+      expect { expect(voucher_feed.run).to be_truthy }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      expect(sftp_session).to have_received(:download!).with("/alma/invoices/abc.xml")
+      expect(sftp_session).to have_received(:rename).with("/alma/invoices/abc.xml", "/alma/invoices/abc.xml.processed")
+      data_set = DataSet.last
+      invoice_errors = "Invalid vendor_id: vendor_id can not be blank, Line Item Invalid: primary fund can not be blank, Line Item Invalid: primary department can not be blank, "\
+                       "Invalid reporting code: must be numeric and can not be blank, Invalid invoice date: must be between four years old and one month into the future"
+      expect(data_set.category).to eq("VoucherFeed")
+      expect(data_set.data).to eq("Lib Vendor Invoice Date,Invoice No,Vendor Code,Vendor Id,Invoice Amount,Invoice Curency,Local Amount,Voucher ID,Errors\n"\
+                                  "1996-03-30,PO-9999,XXX,\"\",1319.05,GBP,176.66,A1222333,\"#{invoice_errors}\"\n")
+      expect(data_set.report_time).to eq(Time.zone.now.midnight)
+      expect(File.exist?("/tmp/alma_voucher_#{today}.XML")).to be_falsey
+      expect(File.exist?("/tmp/Library Invoice Keyword Update_#{onbase_today}.csv")).to be_falsey
+      confirm_email = ActionMailer::Base.deliveries.last
+      expect(confirm_email.subject).to eq("Alma to Peoplesoft Voucher Feed Results")
+      expect(confirm_email.html_part.body.to_s).to include("1996-03-30</td><td>PO-9999</td><td>XXX</td><td></td><td>1319.05</td><td>GBP</td><td>176.66</td><td>A1222333</td>"\
+                                                           "<td>#{invoice_errors}</td>")
+      expect(confirm_email.html_part.body.to_s).to include("No invoices available to process")
+    end
   end
 end
