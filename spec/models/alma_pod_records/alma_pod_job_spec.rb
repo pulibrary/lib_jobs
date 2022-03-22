1dff81b26436d6e75269ddce327f34487b821836
Jane Sandberg <js7389@princeton.edu>
2022-03-22 16:25:05 -0700

    Tweak alma pod job spec

--- spec/models/alma_pod_records/alma_pod_job_spec.rb
+++ spec/models/alma_pod_records/alma_pod_job_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe AlmaPodRecords::AlmaPodJob, type: :model do
     document = File.open(file_fixture('marcxml_no_namespaces.xml')) { |file| Nokogiri::XML(file) }
     list = AlmaPodRecords::AlmaPodFileList.new(documents: [document])
     job = described_class.new(incoming_file_list: list)
-    expect(job.documents.first.children.first.namespace.href).to eq('http://www.loc.gov/MARC21/slim')
+    expect(job.documents.first.xpath('//marc:collection', 'marc' => 'http://www.loc.gov/MARC21/slim').count).to eq(1)
   end
 end
