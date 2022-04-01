dccd96655bae690c3d881f8d335ab76989a50a53
Jane Sandberg <js7389@princeton.edu>
2022-04-01 16:04:09 -0700

    Configurable download directory for POD records

--- app/models/alma_pod_records/alma_pod_job.rb
+++ app/models/alma_pod_records/alma_pod_job.rb
@@ -3,10 +3,11 @@ module AlmaPodRecords
   class AlmaPodJob < LibJob
     attr_reader :documents
 
-    def initialize(incoming_file_list: nil, file_pattern: '\.tar\.gz$', since: nil)
+    def initialize(incoming_file_list: nil, file_pattern: '\.tar\.gz$', since: nil, directory: nil)
       super(category: 'AlmaPodRecords')
       since ||= Rails.application.config.pod.days_to_fetch.days.ago
       @file_list = incoming_file_list || AlmaPodFileList.new(file_pattern: file_pattern, since: since)
+      @download_dir = Pathname.new(directory || Rails.application.config.pod.pod_record_path)
       clean_documents
     end
 
@@ -17,10 +18,10 @@ module AlmaPodRecords
 
     def write_files
       timestamp = Time.zone.now.strftime('%Y-%m-%d-%H-%M-%S')
-      @documents.each_with_index do |document, index|
-        filename = "pod_clean.#{timestamp}.#{index}.xml"
+      @documents.map.with_index do |document, index|
+        filename = @download_dir + "pod_clean.#{timestamp}.#{index}.xml"
         File.write(filename, document.to_xml)
-        filename
+        filename.to_s
       end
     end
 
