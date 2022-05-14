647990a8cfef3330153b9313b10c9e6206284717
Jane Sandberg <sandbergja@users.noreply.github.com>
2022-05-13 18:40:33 -0700

    Don't send empty files to POD (#309)

---
+++ spec/models/alma_pod_records/alma_pod_file_list_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe AlmaPodRecords::AlmaPodFileList, type: :model do
+  subject(:alma_pod_file_list) { described_class.new }
+
+  let(:name) { 'file.tar.gz' }
+  let(:mtime) { Time.now.to_i }
+  let(:file_name) { instance_double("Net::SFTP::Protocol::V01::Name", name: name) }
+  let(:file_attributes) { instance_double("Net::SFTP::Protocol::V01::Attributes") }
+  let(:sftp_session) { instance_double("Net::SFTP::Session", dir: sftp_dir) }
+  let(:sftp_dir) { instance_double("Net::SFTP::Operations::Dir") }
+  let(:file_size) { 1028 }
+  let(:tarball) { instance_double(Tarball) }
+
+  before do
+    allow(file_attributes).to receive(:size).and_return file_size
+    allow(file_attributes).to receive(:mtime).and_return mtime
+    allow(file_name).to receive(:attributes).and_return file_attributes
+    allow(sftp_dir).to receive(:foreach).and_yield file_name
+    allow(sftp_session).to receive(:file).and_return StringIO
+    allow(Net::SFTP).to receive(:start).and_yield sftp_session
+    allow(Tarball).to receive(:new).and_return tarball
+    allow(tarball).to receive(:contents).and_return Array(1)
+  end
+
+  describe 'if the file is valid' do
+    it "downloads the file" do
+      expect(alma_pod_file_list.documents.count).to eq(1)
+    end
+  end
+
+  describe "if the filename doesn't match the pattern" do
+    let(:name) { 'BAD' }
+    it "doesn't download the file" do
+      expect(alma_pod_file_list.documents).to be_empty
+    end
+  end
+
+  describe 'if the file is empty' do
+    let(:file_size) { 0 }
+    it "doesn't download the file" do
+      expect(alma_pod_file_list.documents).to be_empty
+    end
+  end
+
+  describe 'if the file is old' do
+    let(:mtime) { Time.new(1980).to_i }
+    it "doesn't download the file" do
+      expect(alma_pod_file_list.documents).to be_empty
+    end
+  end
+end
