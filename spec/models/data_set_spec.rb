5484b8fca4633c428ebad318627fe969c7dbf3ad
Max Kadel <mkadel@email.unc.edu>
2022-07-01 11:05:19 -0400

    Sort by newest data_set first on index page
    Co-Authored-By: Ryan Laddusaw <rl8282@princeton.edu>

--- spec/models/data_set_spec.rb
+++ spec/models/data_set_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe DataSet, type: :model do
   let(:data_set) { described_class.new }
 
-  it 'has to the expected attributes' do
-    expect(data_set.attributes).to eq({ "category" => nil, "created_at" => nil, "data" => nil, "data_file" => nil, "id" => nil, "report_time" => nil, "updated_at" => nil, "status" => true })
+  it 'has the expected attributes' do
+    expect(data_set.attributes).to eq({ "category" => nil, "created_at" => nil, "data" => nil, "data_file" => nil,
+                                        "id" => nil, "report_time" => nil, "updated_at" => nil, "status" => true })
   end
 end
