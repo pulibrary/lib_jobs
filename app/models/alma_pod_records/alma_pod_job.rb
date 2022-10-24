e56db66fcc2a4af06667cff6ec8998c8863a19bd
Max Kadel <mkadel@princeton.edu>
2022-10-24 13:56:54 -0400

    Factor out file_path method

--- app/models/alma_pod_records/alma_pod_job.rb
+++ app/models/alma_pod_records/alma_pod_job.rb
@@ -20,15 +20,21 @@ module AlmaPodRecords
       timestamp = Time.zone.now.strftime('%Y-%m-%d-%H-%M-%S')
       @file_list.files.each_with_index do |remote_filename, remote_index|
         @file_list.download_and_decompress_file(remote_filename).each_with_index do |contents, tarball_index|
-          file_path = @download_dir + "pod_clean.#{timestamp}.#{remote_index}.#{tarball_index}.xml"
-          final_file_path = write_file(file_path: file_path, contents: contents, compressed: @compressed)
-          AlmaPodSender.new(filename: final_file_path, compressed: @compressed).send_to_pod
+          file_path = file_path(timestamp: timestamp, remote_index: remote_index, tarball_index: tarball_index)
+          write_file(file_path: file_path, contents: contents)
+          AlmaPodSender.new(filename: file_path, compressed: @compressed).send_to_pod
         end
       end
     end
 
-    def write_file(file_path:, contents:, compressed: false)
-      if compressed
+    def file_path(timestamp:, remote_index:, tarball_index:)
+      file_name = "pod_clean.#{timestamp}.#{remote_index}.#{tarball_index}.xml"
+      file_name += '.gz' if @compressed
+      @download_dir + file_name
+    end
+
+    def write_file(file_path:, contents:)
+      if @compressed
         write_compressed_file(file_path: file_path, contents: contents)
       else
         write_uncompressed_file(file_path: file_path, contents: contents)
@@ -37,16 +43,12 @@ module AlmaPodRecords
 
     def write_uncompressed_file(file_path:, contents:)
       MarcCollection.new(contents).write(File.open(file_path, 'w'))
-      file_path
     end
 
     def write_compressed_file(file_path:, contents:)
-      compressed_file_path = file_path.to_path + '.gz'
-      # write gzipped file
-      Zlib::GzipWriter.open(compressed_file_path) do |gz|
+      Zlib::GzipWriter.open(file_path) do |gz|
         MarcCollection.new(contents).write(gz)
       end
-      compressed_file_path
     end
   end
 end
--- spec/models/alma_pod_records/alma_pod_job_spec.rb
+++ spec/models/alma_pod_records/alma_pod_job_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe AlmaPodRecords::AlmaPodJob, type: :model do
 
       it 'writes new xml files' do
         expect(File.exist?(file_path)).to be false
-        alma_pod_job.write_file(file_path: file_path, contents: tarball_contents.first, compressed: compressed)
+        alma_pod_job.write_file(file_path: file_path, contents: tarball_contents.first)
         expect(File.exist?(file_path)).to be true
         expect(File.extname(file_path)).to eq('.xml')
       end
@@ -59,7 +59,6 @@ RSpec.describe AlmaPodRecords::AlmaPodJob, type: :model do
       let(:compressed) { true }
       let(:zipped_file_path) { Pathname.new(Rails.root.join('tmp', "test_file.xml.gz")) }
       let(:alma_pod_job) { described_class.new(incoming_file_list: list, directory: Rails.root.join('tmp'), compressed: true) }
-
 
       around do |example|
         File.delete(file_path) if File.exist?(file_path)
@@ -72,7 +71,7 @@ RSpec.describe AlmaPodRecords::AlmaPodJob, type: :model do
       it 'writes new gz files' do
         expect(File.exist?(file_path)).to be false
         expect(File.exist?(zipped_file_path)).to be false
-        alma_pod_job.write_file(file_path: file_path, contents: tarball_contents.first, compressed: compressed)
+        alma_pod_job.write_file(file_path: zipped_file_path, contents: tarball_contents.first)
         expect(File.exist?(file_path)).to be false
         expect(File.exist?(zipped_file_path)).to be true
         expect(File.extname(zipped_file_path)).to eq('.gz')
