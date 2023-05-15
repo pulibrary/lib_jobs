f33f131247c86ef614d24fd7a092bc7db0f22d7b
regineheberlein <regineheberlein@gmail.com>
2023-05-15 14:29:01 -0400

    rubocop--some errors remain for now

--- app/models/aspace_svn/get_eads_job.rb
+++ app/models/aspace_svn/get_eads_job.rb
@@ -1,84 +1,87 @@
+# frozen_string_literal: true
 require 'archivesspace/client'
 require 'nokogiri'
 require 'fileutils'
 
 module AspaceSvn
   class GetEadsJob < LibJob
-    def initialize()
-      super(category:"EAD_export")
+    def initialize
+      super(category: "EAD_export")
       @errors = []
     end
-    def aspace_login()
-      #configure access
+
+    def aspace_login
+      # configure access
       @config = ArchivesSpace::Configuration.new({
-        base_uri: ENV['ASPACE_URL'],
-        base_repo: "",
-        username: ENV['ASPACE_USER'],
-        password: ENV['ASPACE_PASSWORD'],
-        #page_size: 50,
-        throttle: 0,
-        verify_ssl: false
-      })
-      #log in
+                                                   base_uri: ENV['ASPACE_URL'],
+                                                   base_repo: "",
+                                                   username: ENV['ASPACE_USER'],
+                                                   password: ENV['ASPACE_PASSWORD'],
+                                                   # page_size: 50,
+                                                   throttle: 0,
+                                                   verify_ssl: false
+                                                 })
+      # log in
       @client = ArchivesSpace::Client.new(@config).login
     end
+
     def handle(data_set:)
       aspace_login
       repos = (3..12).to_a
       repos.each do |repo|
-
         # make directories if they don't already exist
         dir =
           case repo
-            when 3
-              "mudd/publicpolicy"
-            when 4
-              "mudd/univarchives"
-            when 5
-              "mss"
-            when 6
-              "rarebooks"
-            when 7
-              "cotsen"
-            when 8
-              "lae"
-            when 9
-              "eng"
-            when 10
-              "selectors"
-            when 11
-              "ga"
-            when 12
-              "ea"
+          when 3
+            "mudd/publicpolicy"
+          when 4
+            "mudd/univarchives"
+          when 5
+            "mss"
+          when 6
+            "rarebooks"
+          when 7
+            "cotsen"
+          when 8
+            "lae"
+          when 9
+            "eng"
+          when 10
+            "selectors"
+          when 11
+            "ga"
+          when 12
+            "ea"
           end
-        FileUtils.mkdir_p(dir) unless Dir.exists?(dir)
+        FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
 
-        #get resource ids
+        # get resource ids
         resource_ids = @client.get("/repositories/#{repo}/resources", {
-          query: {
-           all_ids: true
-         }}).parsed
-         #get eads from ids
+                                     query: {
+                                       all_ids: true
+                                     }
+                                   }).parsed
+        # get eads from ids
         eads =
           resource_ids.map do |id|
             record = @client.get("/repositories/#{repo}/resource_descriptions/#{id}.xml")
             ead = Nokogiri::XML(record.body)
-            #i shouldn't have to do this but I do
+            # i shouldn't have to do this but I do
             ead.remove_namespaces!
             eadid = ead.at_xpath('//eadid/text()')
-            #save file with eadid as file name
+            # save file with eadid as file name
             file =  File.open("./#{dir}/#{eadid}.EAD.xml", "w")
-            #add back a default namespace
+            # add back a default namespace
             ead.child.add_namespace('ead', 'http://www.loc.gov/ead/ead')
             file << ead
             file.close
           end
-
       end
       data_set.data = report
       data_set.report_time = Time.zone.now
       data_set
     end
+
     def report
       # item_count = renew_list.renew_item_list.count
       if @errors.empty?
