04f455c5ea889cde0494f6a0d4258eea1d40ceb6
Liz Garcia <lg6248@princeton.edu>
2023-06-29 11:57:50 -0400

    Exclude lux related errors
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Isha Sinha <ishasinha1@users.noreply.github.com>
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -10,6 +10,8 @@ describe "accessibility", type: :system, js: true do
     it "complies with wcag" do
       expect(page).to be_axe_clean
         .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
+        .excluding('duplicate-id-aria') # https://github.com/pulibrary/lib_jobs/issues/538 
+        .excluding('.lux-main-menu-list') # https://github.com/pulibrary/lib_jobs/issues/541
     end
   end
 end
