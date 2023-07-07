a1c66b4b9844d2bd16afcc6e0bbf8fdf1cdfe48f
Max Kadel <mkadel@princeton.edu>
2023-07-07 16:11:16 -0400

    Add OclcSftp class
    - Not very dry right now, would like to refactor with AlmaSftp class and pull out an Sftp class

--- app/models/oclc_sftp.rb
+++ app/models/oclc_sftp.rb
@@ -1,3 +1,26 @@
 # frozen_string_literal: true
 class OclcSftp
+  attr_reader :sftp_host, :sftp_username
+
+  def initialize(sftp_host: Rails.application.config.oclc_sftp.host,
+                 sftp_username: Rails.application.config.oclc_sftp.username,
+                 sftp_password: Rails.application.config.oclc_sftp.password)
+    @sftp_host = sftp_host
+    @sftp_username = sftp_username
+    @sftp_password = sftp_password
+  end
+
+  def start
+    retries ||= 0
+    Net::SFTP.start(sftp_host, sftp_username, { password: @sftp_password }) do |sftp|
+      yield(sftp)
+    end
+  rescue Net::SSH::Disconnect => error
+    while (retries += 1) <= 3
+      Rails.logger.warn("Encountered #{error.class}: '#{error.message}' when connecting at #{Time.now.utc}, retrying in #{retries} second(s)...")
+      sleep(retries)
+      retry
+    end
+    Rails.logger.error("Encountered #{error.class}: '#{error.message}' at #{Time.now.utc}, unsuccessful in connecting after #{retries} retries")
+  end
 end
