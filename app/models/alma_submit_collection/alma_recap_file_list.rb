5f3c4cf476b452e895e8e0c408b44389fb6b7a7f
Jane Sandberg <js7389@princeton.edu>
2023-07-21 10:52:21 -0700

    Use sftp, rather than ftp, to maintain consistency with the rest of lib_jobs

--- app/models/alma_submit_collection/alma_recap_file_list.rb
+++ app/models/alma_submit_collection/alma_recap_file_list.rb
@@ -3,8 +3,8 @@ module AlmaSubmitCollection
   class AlmaRecapFileList
     attr_reader :files
 
-    def initialize(input_ftp_base_dir: Rails.application.config.alma_ftp.alma_recap_output_path, file_pattern: '\.tar\.gz$', alma_sftp: AlmaSftp.new, files: nil)
-      @input_ftp_base_dir = input_ftp_base_dir
+    def initialize(input_sftp_base_dir: Rails.application.config.alma_sftp.alma_recap_output_path, file_pattern: '\.tar\.gz$', alma_sftp: AlmaSftp.new, files: nil)
+      @input_sftp_base_dir = input_sftp_base_dir
       @file_pattern = file_pattern
       @alma_sftp = alma_sftp
       @sftp_locations = []
@@ -39,7 +39,7 @@ module AlmaSubmitCollection
     def download_file(filename)
       @alma_sftp.start do |sftp|
         Rails.logger.info "Downloading Alma Recap file #{filename}"
-        full_filename = File.join(@input_ftp_base_dir, filename)
+        full_filename = File.join(@input_sftp_base_dir, filename)
         contents = sftp.file.open(full_filename)
         sftp.rename(full_filename, "#{full_filename}.processed")
         contents
