426baec8f5725e85d8c0c777469bd7fe1a7e8b02
Isha <is4684@princeton.edu>
2023-07-26 17:43:09 -0400

    Remove test exception
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -10,7 +10,6 @@ describe "accessibility", type: :system, js: true do
     it "complies with wcag" do
       expect(page).to be_axe_clean
         .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa)
-        .skipping('duplicate-id-aria') # https://github.com/pulibrary/lib_jobs/issues/538
     end
   end
 end
