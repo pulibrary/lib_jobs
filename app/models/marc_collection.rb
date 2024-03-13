33278a6c767b774b353ce4a8ba43ff9501b6267d
Jane Sandberg <js7389@princeton.edu>
2024-03-13 14:24:52 -0700

    Add more logging to SubmitCollection job
    Co-authored-by: Christina Chortaria <christinach@users.noreply.github.com>
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

--- app/models/marc_collection.rb
+++ app/models/marc_collection.rb
@@ -11,6 +11,7 @@ class MarcCollection
   end
 
   def write(io)
+    Rails.logger.debug('Parsing an XML file and adding the namespace')
     parser = Nokogiri::XML::SAX::Parser.new(MarcCollectionDocumentCallbacks.new(io))
     io.write "<?xml version=\"1.0\"?>\n"
     parser.parse document
