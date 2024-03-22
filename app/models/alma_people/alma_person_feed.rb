af9ffc95d84c0ee3da1941f1e705512334888cb1
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-03-22 15:53:35 -0400

    Added new call to generate ineligible user file

--- app/models/alma_people/alma_person_feed.rb
+++ app/models/alma_people/alma_person_feed.rb
@@ -5,13 +5,14 @@ module AlmaPeople
 
     def initialize(oit_person_feed: AlmaPeople::OitPersonFeed.new, begin_date: 1.day.ago.strftime("%Y-%m-%d"),
                    end_date: Time.zone.now.strftime("%Y-%m-%d"), output_base_dir: ENV["ALMA_PERSON_FEED_OUTPUT_DIR"] || '/tmp',
-                   enabled_flag: 'E')
+                   enabled_flag: 'E', ineligible_flag: 'I')
       super(category: "AlmaPersonFeed")
       @oit_person_feed = oit_person_feed
       @begin_date = begin_date
       @end_date = end_date
       @output_base_dir = output_base_dir
       @enabled_flag = enabled_flag
+      @ineligible_flag = ineligible_flag
       @invalid_records = []
     end
 
@@ -20,14 +21,16 @@ module AlmaPeople
     def handle(data_set:)
       data_set.report_time = Time.zone.now.midnight
       oit_people = oit_person_feed.get_json(begin_date:, end_date:, enabled_flag:)
-      full_path = build_xml(oit_people:)
+      full_path = build_xml(oit_people:, eligibility: enabled_flag)
       transfer_alma_person_file(filename: full_path)
+      ineligible_user_message = ineligible_user if Flipflop.alma_person_ineligible?
       AlmaPeopleMailer.error_notification(invalid_records: @invalid_records).deliver
       data_set.data = "people_updated: #{oit_people.count}, file: #{File.basename(full_path)}"
+      data_set.data << ineligible_user_message if Flipflop.alma_person_ineligible?
       data_set
     end
 
-    def build_xml(oit_people:)
+    def build_xml(oit_people:, eligibility:)
       return "" if oit_people.empty?
 
       builder = Nokogiri::XML::Builder.new do |xml|
@@ -37,7 +40,7 @@ module AlmaPeople
           end
         end
       end
-      full_path = File.join(output_base_dir, output_filename)
+      full_path = File.join(output_base_dir, output_filename(eligibility:))
       File.open(full_path, "w") do |file|
         file.write builder.to_xml
       end
@@ -65,10 +68,17 @@ module AlmaPeople
       end
     end
 
-    def output_filename
+    def output_filename(eligibility:)
       date = begin_date || 1.day.ago.strftime("%Y-%m-%d")
-      fname = ["alma_people", date, end_date, enabled_flag].compact.join('_')
+      fname = ["alma_people", date, end_date, eligibility].compact.join('_')
       fname + ".xml"
+    end
+
+    def ineligible_user
+      oit_people_ineligible = oit_person_feed.get_json(begin_date:, end_date:, enabled_flag: @ineligible_flag)
+      full_path = build_xml(oit_people: oit_people_ineligible, eligibility: @ineligible_flag)
+      transfer_alma_person_file(filename: full_path)
+      ", ineligible_people_updated: #{oit_people_ineligible.count}, file: #{File.basename(full_path)}"
     end
   end
 end
