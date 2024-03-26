0194d3075b4928d8764a8a52ef883834f5f6d7e3
Max Kadel <mkadel@princeton.edu>
2024-03-26 13:39:46 -0400

    Allow less secure algorithms for some SFTP classes
    - The Gobi sftp server does not have modern algorithms available to authenticate with, so we have to append all supported algorithms, some of which are less secure
    - Default to not appending the less secure algorithms
    - Refactor SFTP specs to use shared examples when appropriate, add an SFTP spec for parent class
    - Update Gobi sftp config, make sure they work for test and dev as well

--- app/models/sftp.rb
+++ app/models/sftp.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 class Sftp
   attr_reader :sftp_host, :sftp_username
-  def initialize(sftp_host:, sftp_username:, sftp_password:)
+  def initialize(sftp_host:, sftp_username:, sftp_password:, allow_less_secure_algorithms: false)
     @sftp_host = sftp_host
     @sftp_username = sftp_username
     @sftp_password = sftp_password
+    @allow_less_secure_algorithms = allow_less_secure_algorithms
   end
 
   def start
     retries ||= 0
-    Net::SFTP.start(sftp_host, sftp_username, { password: @sftp_password }) do |sftp|
+    Net::SFTP.start(sftp_host, sftp_username,
+      { password: @sftp_password, append_all_supported_algorithms: @allow_less_secure_algorithms }) do |sftp|
       yield(sftp)
     end
   rescue Net::SSH::Disconnect => error
