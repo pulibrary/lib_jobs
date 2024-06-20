6bd8c33d308aea7602e16caef9d7b57d61552695
Max Kadel <mkadel@princeton.edu>
2024-06-20 13:06:21 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'lib-ponyexpr-prod.princeton.edu'
+    address: 'lib-ponyexpr-prod.princeton.edu',
+    enable_starttls: false
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
