c0bb662ea535cd3155cbfd4eb0d1868fa0c60aa0
Max Kadel <mkadel@princeton.edu>
2024-07-24 13:40:27 -0400

    Update column for first name from airtable (#820)
    Closes #819

--- app/models/air_table_staff/staff_directory_mapping.rb
+++ app/models/air_table_staff/staff_directory_mapping.rb
@@ -18,7 +18,7 @@ module AirTableStaff
         { airtable_field: :'University Phone', airtable_field_id: :fldqulY6ehd5aIbR1, our_field: :phone },
         { airtable_field: :'pul:Preferred Name', airtable_field_id: :fldL7tm4jVvYksIwl, our_field: :name },
         { airtable_field: :'Last Name', airtable_field_id: :fldcGj6p3JRzHzmZ8, our_field: :lastName },
-        { airtable_field: :'First Name', airtable_field_id: :fldL5IJdMuz8UCM81, our_field: :firstName },
+        { airtable_field: :'pul:First Name', airtable_field_id: :fldnKprqGraSvNTJK, our_field: :firstName },
         { airtable_field: :Email, airtable_field_id: :fldbnDHHhDNlc2Lx8, our_field: :email },
         { airtable_field: :Address, airtable_field_id: :fldKZxmtofNbXW4qS, our_field: :address },
         { airtable_field: :'pul:Building', airtable_field_id: :fldz6yBenvTjdClXZ, our_field: :building },
