ca3bae554ca844ded9d6bad74018568b3055f784
Christina Chortaria <actspatial@gmail.com>
2026-01-08 15:13:01 -0500

    commit db/schema changes rubocop fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_12_181445) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_12_181445) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "data_sets", force: :cascade do |t|
     t.datetime "created_at", precision: nil, null: false
