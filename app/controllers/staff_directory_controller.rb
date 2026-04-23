f2e2b91a3033d38076e479b48ed452dc09b75876
rladdusaw <rladdusaw@gmail.com>
2026-04-23 14:47:38 -0400

    Remove web staff job and related files (#1056)
    Co-authored-by: Ryan Laddusaw <rladdusaw@princeton.edu>

--- app/controllers/staff_directory_controller.rb
+++ app/controllers/staff_directory_controller.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 class StaffDirectoryController < ApplicationController
-  def index
-    hr_report = WebStaff::HrStaffReport.new
-    generator = WebStaff::StaffDirectoryGenerator.new(hr_report:)
-    respond_to do |format|
-      format.csv { send_data generator.today, filename: "staff-directory.csv" }
-    end
-  end
-
-  def removed
-    today_report = File.new(WebStaff::StaffDirectoryGenerator.report_filename)
-    yesterday_report = File.new(WebStaff::StaffDirectoryGenerator.yesterday_filename)
-    differ = WebStaff::StaffDirectoryDifference.new(new_report: today_report, old_report: yesterday_report)
-    respond_to do |format|
-      format.text { send_data differ.ids.join(","), filename: "removed-staff.txt" }
-    end
-  end
-
   def pul_staff_report
     if Flipflop.air_table_staff_list?
       job = AirTableStaff::StaffListJob.new
--- lib/tasks/lib_jobs.rake
+++ lib/tasks/lib_jobs.rake
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 namespace :lib_jobs do
-  desc "generate the daily staff report"
-  task generate_staff_report: [:environment] do
-    generator = WebStaff::StaffDirectoryGenerator.new(hr_report: WebStaff::HrStaffReport.new)
-    generator.today
-    today_report = File.new(WebStaff::StaffDirectoryGenerator.report_filename)
-    yesterday_report = File.new(WebStaff::StaffDirectoryGenerator.yesterday_filename)
-    differ = WebStaff::StaffDirectoryDifference.new(new_report: today_report, old_report: yesterday_report)
-    differ.ids
-  end
-
   desc "generate the events feed csv"
   task generate_events_csv: [:environment] do
     generator = WebEvents::EventsFeedGenerator.new
