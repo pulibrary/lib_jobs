67be3b5894c9bb2453af6aa43a2e26b90e341b6d
Jane Sandberg <js7389@princeton.edu>
2026-05-20 15:21:52 -0700

    Remove JWT tests

--- spec/requests/data_set_request_spec.rb
+++ spec/requests/data_set_request_spec.rb
@@ -113,53 +113,6 @@ RSpec.describe "DataSets", type: :request do
       end
     end
 
-    context "when the client is authenticated via a token" do
-      let(:user) do
-        User.create(email: 'user@localhost')
-      end
-
-      let(:headers) do
-        {
-          "Accept" => "application/json",
-          "Authorization" => "Bearer #{user.token}"
-        }
-      end
-
-      let(:params) do
-        {
-          user: { id: user.id }
-        }
-      end
-
-      before do
-        user
-        DataSet.create(report_time: today_end_of_hour, data: "abc123,def456", data_file: nil, status: true, category: "StaffDirectoryRemoved")
-        DataSet.create(report_time: today_end_of_hour, data: "other5,other6", data_file: nil, status: false, category: "Other")
-        DataSet.create(report_time: today, data: "other3,other4", data_file: nil, status: true, category: "Other")
-        DataSet.create(report_time: previous_time, data: "other1,other2", data_file: nil, status: true, category: "Other")
-      end
-
-      it "returns the latest other dataset" do
-        get("/data_sets/latest/other", headers:, params:)
-        expect(response.body).to eq('other3,other4')
-      end
-
-      it "returns the StaffDirectoryRemoved dataset" do
-        get("/data_sets/latest/staff-directory-removed", headers:, params:)
-        expect(response.body).to eq('abc123,def456')
-      end
-
-      it "returns the data in a file for StaffDirectory" do
-        Tempfile.create do |file|
-          file.write("data from file\nMultiple lines")
-          file.rewind
-          DataSet.create(report_time: today, data: nil, data_file: file.path, status: true, category: "StaffDirectory")
-          get("/data_sets/latest/staff-directory", headers:, params:)
-          expect(response.body).to eq("data from file\nMultiple lines")
-        end
-      end
-    end
-
     context "when the client is authenticated via cas" do
       let(:user) do
         User.create(email: 'user@localhost')
