804e6ccf679a9f0e62644a75d4a0391dd84e39ec
Jane Sandberg <js7389@princeton.edu>
2026-08-18 15:27:55 -0700

    Move a small class to Hanami

--- slices/air_table_staff/json_value_extractor.rb
+++ slices/air_table_staff/json_value_extractor.rb
@@ -3,20 +3,11 @@ module AirTableStaff
   # This class is responsible for extracting a single
   # value from a json hash, based on the criteria in
   # the field hash
-  class JsonValueExtractor
-    def initialize(json:, field:)
-      @json = json
-      @field = field
-    end
-
-    def extract
+  class JSONValueExtractor
+    def call(json:, field:)
       raw_value = json[field[:airtable_field_id]]
       transformer = field[:transformer]
       transformer ? transformer.call(raw_value) : raw_value
     end
-
-    private
-
-    attr_reader :json, :field
   end
 end
--- spec/slices/air_table_staff/json_value_extractor_spec.rb
+++ spec/slices/air_table_staff/json_value_extractor_spec.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe AirTableStaff::JsonValueExtractor do
+RSpec.describe AirTableStaff::JSONValueExtractor do
   context 'when there is no transformer lambda provided in field config' do
     it 'extracts the text verbatim' do
       json = { cat: 'tabby cat' }
       field = { airtable_field_id: :cat }
-      expect(described_class.new(json:, field:).extract).to eq('tabby cat')
+      expect(described_class.new.call(json:, field:)).to eq('tabby cat')
     end
   end
   context 'when there is a transformer lambda provided in field config' do
     it 'extracts the text verbatim' do
       json = { cat: 'tabby cat' }
       field = { airtable_field_id: :cat, transformer: ->(value) { "My favorite cat is #{value}" } }
-      expect(described_class.new(json:, field:).extract).to eq('My favorite cat is tabby cat')
+      expect(described_class.new.call(json:, field:)).to eq('My favorite cat is tabby cat')
     end
   end
 end
